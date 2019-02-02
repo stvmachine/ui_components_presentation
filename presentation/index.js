1c42efcddd8d3b848ccbffe94a86c7fd3a2fdb85
stevmachine <campos.esteban@gmail.com>
2019-02-02 15:33:48 +1100

    modifying slides

--- presentation/index.js
+++ presentation/index.js
@@ -1,67 +1,59 @@
-import React from 'react';
+import React from "react";
 
 // Import Spectacle Core tags
-import {
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Slide,
-  Text,
-  Notes
-} from 'spectacle';
+import { Deck, Heading, ListItem, List, Slide, Text, Notes } from "spectacle";
 
-import Terminal from 'spectacle-terminal';
-import Typist from 'react-typist';
-import { ScaleLoader } from 'react-spinners';
-import CodeSlide from 'spectacle-code-slide';
-import preloader from 'spectacle/lib/utils/preloader';
-import createTheme from 'spectacle/lib/themes/default';
+import Terminal from "spectacle-terminal";
+import Typist from "react-typist";
+import { ScaleLoader } from "react-spinners";
+import CodeSlide from "spectacle-code-slide";
+import preloader from "spectacle/lib/utils/preloader";
+import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
-import 'normalize.css';
-import './style.css';
+import "normalize.css";
+import "./style.css";
 
 const images = {
-  advancedConfig: require('../assets/advanced-config.png'),
-  basicConfig: require('../assets/basic-config.png'),
-  basicStory: require('../assets/basic-story.png'),
-  buildStorybook: require('../assets/build-storybook.png'),
-  customWebpack: require('../assets/custom-webpack.png'),
-  folderStructure: require('../assets/folder-structure.png'),
-  storybookIntro: require('../assets/storybook-intro.gif')
+  advancedConfig: require("../assets/advanced-config.png"),
+  basicConfig: require("../assets/basic-config.png"),
+  basicStory: require("../assets/basic-story.png"),
+  buildStorybook: require("../assets/build-storybook.png"),
+  customWebpack: require("../assets/custom-webpack.png"),
+  folderStructure: require("../assets/folder-structure.png"),
+  storybookIntro: require("../assets/storybook-intro.gif")
 };
 preloader({
   images
 });
 
 const slideProps = {
-  bgColor: 'background',
-  progressColor: 'tertiary',
-  transition: ['fade']
+  bgColor: "background",
+  progressColor: "tertiary",
+  transition: ["fade"]
 };
 
 const cursor = {
   show: false,
   blink: true,
-  element: '|',
+  element: "|",
   hideWhenDone: false,
   hideWhenDoneDelay: 1000
 };
 
-const Tick = () => <span style={{ color: '#33B969' }}>✓</span>;
-const Info = () => <span style={{ color: '#33B969' }}>info</span>;
+const Tick = () => <span style={{ color: "#33B969" }}>✓</span>;
+const Info = () => <span style={{ color: "#33B969" }}>info</span>;
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE'
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quaternary: "#CECECE"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica'
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
@@ -69,42 +61,12 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck contentHeight={1000} contentWidth={1150} theme={theme}>
-        <CodeSlide
-          transition={['fade']}
-          lang="jsx"
-          textSize=".8em"
-          // eslint-disable-next-line import/no-unresolved
-          code={require('raw-loader!../assets/deck.example')}
-          ranges={[
-            {
-              loc: [0, 270],
-              title: 'Walking through some code'
-            },
-            {
-              loc: [0, 1],
-              title: 'The Beginning'
-            },
-            { loc: [1, 2] },
-            {
-              loc: [1, 2],
-              note: 'Heres a note!'
-            },
-            { loc: [2, 3] },
-            { loc: [4, 7] },
-            { loc: [8, 10] },
-            { loc: [11, 15] },
-            { loc: [16, 30] },
-            { loc: [31, 40] },
-            { loc: [41, 50] },
-            { loc: [51, 60] }
-          ]}
-        />
-        <Slide {...slideProps} transition={['zoom']}>
+        <Slide {...slideProps} transition={["zoom"]}>
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
-            Storybook
+            {`Styleguids & playgrounds`}
           </Heading>
           <Text margin="10px 0 0" textColor="secondary" size={1} fit bold>
-            Workshop: introduction and setup
+            why we need more tools?
           </Text>
 
           <Notes>
@@ -120,28 +82,34 @@ export default class Presentation extends React.Component {
             Agenda
           </Heading>
           <List textColor="secondary">
-            <ListItem>Introduction to Storybook</ListItem>
-            <ListItem>Basic setup</ListItem>
-            <ListItem>Custom configuration</ListItem>
-            <ListItem>Testing with Storybook - by Pete Warman</ListItem>
-            <ListItem>Interactive setup demo</ListItem>
-            <ListItem>Workshop - setup your own project</ListItem>
+            <ListItem>Style guide and playgrounds</ListItem>
+            <ListItem>What it is available</ListItem>
+            <ListItem>Storybook</ListItem>
+            <ListItem>Docz</ListItem>
+            <ListItem>React cosmos</ListItem>
           </List>
         </Slide>
         <Slide {...slideProps}>
           <Heading size={1} textColor="primary">
-          Storybook
+            Storybook
           </Heading>
           <List textColor="secondary">
             <ListItem>Project maturity and active community</ListItem>
             <ListItem>Straightforward to setup and maintain</ListItem>
-            <ListItem>Hot module reload (even for functional stateless components)</ListItem>
-            <ListItem>Allows to inspect events related to your components</ListItem>
-            <ListItem>Allows to edit React props dynamically using the Storybook UI</ListItem>
-            <ListItem>Integrates Jest snapshot testing for all stories</ListItem>
+            <ListItem>
+              Hot module reload (even for functional stateless components)
+            </ListItem>
+            <ListItem>
+              Allows to inspect events related to your components
+            </ListItem>
+            <ListItem>
+              Allows to edit React props dynamically using the Storybook UI
+            </ListItem>
+            <ListItem>
+              Integrates Jest snapshot testing for all stories
+            </ListItem>
             <ListItem>Possibility to further customization via Addons</ListItem>
           </List>
-
         </Slide>
         <Slide {...slideProps}>
           <Heading size={1} textColor="primary">
@@ -171,23 +139,23 @@ export default class Presentation extends React.Component {
                 <div
                   key="loader"
                   style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    color: '#9D9D9D'
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    color: "#9D9D9D"
                   }}
                 >
                   <ScaleLoader color="#9D9D9D" loading height={15} />
                   <p
                     style={{
                       margin: 0,
-                      marginLeft: '0.5em'
+                      marginLeft: "0.5em"
                     }}
                   >
                     Installing dependencies...
                   </p>
                 </div>,
-                <div style={{ color: '#33B969' }} key="installed">
+                <div style={{ color: "#33B969" }} key="installed">
                   ⚡️ Dependencies installed!
                 </div>
               ],
@@ -197,8 +165,8 @@ export default class Presentation extends React.Component {
               <p key="simplest-way">
                 <span
                   style={{
-                    backgroundColor: '#9D9D9D',
-                    marginLeft: '5px'
+                    backgroundColor: "#9D9D9D",
+                    marginLeft: "5px"
                   }}
                 >
                   getstorybook - the simplest way to add a storybook to your
@@ -208,8 +176,8 @@ export default class Presentation extends React.Component {
               <span
                 key="step-1"
                 style={{
-                  marginLeft: '5px',
-                  color: '#9D9D9D'
+                  marginLeft: "5px",
+                  color: "#9D9D9D"
                 }}
               >
                 • Detecting project type. <Tick />
@@ -217,8 +185,8 @@ export default class Presentation extends React.Component {
               <span
                 key="step-2"
                 style={{
-                  marginLeft: '5px',
-                  color: '#9D9D9D'
+                  marginLeft: "5px",
+                  color: "#9D9D9D"
                 }}
               >
                 • Adding storybook support to your "Create React App" based
@@ -227,8 +195,8 @@ export default class Presentation extends React.Component {
               <span
                 key="step-3"
                 style={{
-                  marginLeft: '5px',
-                  color: '#9D9D9D'
+                  marginLeft: "5px",
+                  color: "#9D9D9D"
                 }}
               >
                 • Preparing to install dependencies. <Tick />
@@ -236,9 +204,9 @@ export default class Presentation extends React.Component {
               <div
                 key="yarn-install"
                 style={{
-                  marginLeft: '10px',
-                  lineHeight: '10px',
-                  color: '#9D9D9D'
+                  marginLeft: "10px",
+                  lineHeight: "10px",
+                  color: "#9D9D9D"
                 }}
               >
                 <p>yarn install v1.5.1</p>
@@ -247,7 +215,7 @@ export default class Presentation extends React.Component {
                 <p>[3/4] 🔗 Linking dependencies...</p>
                 <p>[4/4] 📃 Building fresh packages...</p>
                 <p>
-                  <span style={{ color: '#33B969' }}>success</span> Saved
+                  <span style={{ color: "#33B969" }}>success</span> Saved
                   lockfile.
                 </p>
                 <p>✨ Done in 20.59s.</p>
@@ -255,8 +223,8 @@ export default class Presentation extends React.Component {
               <span
                 key="step-4"
                 style={{
-                  marginLeft: '5px',
-                  color: '#9D9D9D'
+                  marginLeft: "5px",
+                  color: "#9D9D9D"
                 }}
               >
                 • Installing dependencies. <Tick />
@@ -264,23 +232,23 @@ export default class Presentation extends React.Component {
               <div
                 key="step-5"
                 style={{
-                  marginLeft: '5px',
-                  color: '#9D9D9D'
+                  marginLeft: "5px",
+                  color: "#9D9D9D"
                 }}
               >
                 <p>To run your storybook, type:</p>
                 <span
                   style={{
-                    marginLeft: '10px',
-                    backgroundColor: '#9D9D9D',
-                    color: '#FFFFFF'
+                    marginLeft: "10px",
+                    backgroundColor: "#9D9D9D",
+                    color: "#FFFFFF"
                   }}
                 >
                   yarn run storybook
                 </span>
                 <p>
-                  For more information visit:{' '}
-                  <a style={{ color: '#21ADDD' }}>https://storybook.js.org</a>
+                  For more information visit:{" "}
+                  <a style={{ color: "#21ADDD" }}>https://storybook.js.org</a>
                 </p>
               </div>,
               [
@@ -291,13 +259,13 @@ export default class Presentation extends React.Component {
                 <div
                   key="yarn-run"
                   style={{
-                    marginLeft: '10px',
-                    lineHeight: '10px',
-                    color: '#9D9D9D'
+                    marginLeft: "10px",
+                    lineHeight: "10px",
+                    color: "#9D9D9D"
                   }}
                 >
                   <p>yarn run v1.5.1</p>
-                  <p style={{ color: '#707070' }}>
+                  <p style={{ color: "#707070" }}>
                     $ start-storybook -p 9009 -s public
                   </p>
                   <p>
@@ -319,8 +287,8 @@ export default class Presentation extends React.Component {
                   </p>
                   <p>webpack built ff898fa748423117d134 in 5901ms</p>
                   <p>
-                    <Info /> Storybook started on =>{' '}
-                    <a style={{ color: '#21ADDD' }}>http://localhost:9009/</a>
+                    <Info /> Storybook started on =>{" "}
+                    <a style={{ color: "#21ADDD" }}>http://localhost:9009/</a>
                   </p>
                 </div>
               ],
@@ -369,76 +337,42 @@ export default class Presentation extends React.Component {
           </Heading>
           <img src={images.buildStorybook} width="100%" />
         </Slide>
-        <Slide {...slideProps}>
-          <Heading size={1} textColor="primary" margin="0 0 50px">
-            Static export
-          </Heading>
-          <Terminal
-            isMaximized={false}
-            title="stevmachine@storybook: ~"
-            output={[
-              <Typist key="yarn-build-storybook" cursor={cursor}>
-                yarn build-storybook
-              </Typist>,
-              <div
-                key="build-storybook"
-                style={{
-                  marginLeft: '10px',
-                  lineHeight: '10px',
-                  color: '#9D9D9D'
-                }}
-              >
-                <p>yarn run v1.5.1</p>
-                <p style={{ color: '#707070' }}>$ build-storybook -s public</p>
-                <p>
-                  <Info /> @storybook/react v3.4.1
-                </p>
-                <p>
-                  <Info />
-                </p>
-                <p>
-                  <Info /> => Loading static files from:
-                  /Users/oritorri/Sapient/workshops/test/public .
-                </p>
-                <p>
-                  <Info /> => Loading custom addons config.
-                </p>
-                <p>
-                  <Info /> => Using default webpack setup based on "Create React
-                  App".
-                </p>
-                <p>
-                  <Info /> => Copying static files from: public
-                </p>
-                <p>
-                  <Info /> Building storybook ...
-                </p>
-              </div>,
-              <div
-                key="building-storybook"
-                style={{
-                  marginLeft: '10px'
-                }}
-              >
-                <p>
-                  <Info /> Building storybook completed.
-                </p>
-                <p>✨ Done in 21.83s.</p>
-              </div>
-            ]}
-          />
-        </Slide>
-        <Slide {...slideProps}>
-          <Heading size={2} textColor="primary">
-            Testing with
-          </Heading>
-          <Heading size={1} textColor="primary" caps bold>
-            Storyboook
-          </Heading>
-          <Text margin="10px 0 0" textColor="secondary" bold>
-            By Pete Warman
-          </Text>
-        </Slide>
+        <CodeSlide
+          transition={["fade"]}
+          lang="jsx"
+          textSize=".8em"
+          // eslint-disable-next-line import/no-unresolved
+          code={require("raw-loader!../assets/deck.example")}
+          ranges={[
+            {
+              loc: [0, 270],
+              title: "Walking through some code"
+            },
+            {
+              loc: [0, 4],
+              title: "The beginning"
+            },
+            {
+              loc: [5, 8],
+              title: "Necessary dependencies"
+            },
+            {
+              loc: [8, 19],
+              title: "Comments"
+            },
+            {
+              loc: [19, 22],
+              title: "Props Table"
+            },
+            {
+              loc: [23, 29],
+              title: "Basic Usage"
+            },
+            { loc: [29, 37] },
+            { loc: [37, 47] },
+            { loc: [47, 57] }
+          ]}
+        />
       </Deck>
     );
   }
